8b6ab5b9497d12c3fc7fab3af903a2654d570eec
Rana <ronaashraf1@hotmail.com>
2020-11-21 08:18:29 +0200

    cookies

--- app.js
+++ app.js
@@ -31,36 +31,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
+//Authorization middleware
 function auth(req, res ,next){
 
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
+  console.log(req.signedCookies);
 
-  if(! authHeader){
-    var err = new Error(' You are not Authorized!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+  if (!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
 
-  }
-
-  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-
-  var username= auth[0];
-  var password = auth[1];
-
-  if (username ==='admin' && password ==='password'){
-    next();
+    if(! authHeader){
+      var err = new Error(' You are not Authorized!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      return next(err);
+  
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+  
+    var username= auth[0];
+    var password = auth[1];
+  
+    if (username ==='admin' && password ==='password'){
+      res.cookie('user','admin',{ signed:true});
+      next();
+    }
+    else{
+      var err = new Error(' You are not Authorized!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      return next(err);
+    }
+  
   }
   else{
-    var err = new Error(' You are not Authorized!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+    if(req.signedCookies.user=== 'admin'){
+      next();
+    }
+    else{
+      var err = new Error(' You are not Authorized!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401;
+      return next(err);
+    }
   }
-
+  
 }
 
 app.use(auth);
